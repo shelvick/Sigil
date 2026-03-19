f7797c6b010ce2e31efa0eb7e1a3caed0525aa17
Scott Helvick <4016577+shelvick@users.noreply.github.com>
2026-03-19 23:28:17 +0000

    Add Gigalixir deployment config

--- config/runtime.exs
+++ config/runtime.exs
@@ -42,11 +42,18 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "sigil.fly.dev"
+  host =
+    System.get_env("PHX_HOST") ||
+      raise """
+      environment variable PHX_HOST is missing.
+      For example: sigil.gigalixirapp.com
+      """
+
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :sigil, SigilWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [ip: {0, 0, 0, 0, 0, 0, 0, 0}, port: port],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    server: true
 end
